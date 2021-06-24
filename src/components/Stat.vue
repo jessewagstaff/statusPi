fb31f1b26308b0ba16c14037e89a725b6881504a
Jesse Wagstaff <git@jessewagstaff.com>
2021-06-23 18:52:05 -0700

    Maintain the daily min/max?

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -2,32 +2,42 @@
   <section>
     <header>{{ title }}</header>
     <div>{{ value === null ? 'N/A' : value.toFixed(fixed) }}</div>
-    <SparklineAreaChart v-if="chartData.length > 1" class="Stat_Chart" :data="chartData" :height="150" />
+    <SparklineAreaChart
+      v-if="chartData.length > 1"
+      class="Stat_Chart"
+      :data="chartData"
+      :height="150"
+      :max="max"
+      :min="min"
+    />
   </section>
 </template>
 <script setup>
 import { defineProps, ref, watch } from 'vue';
 import heartbeat from '../heartbeat';
-import SparklineAreaChart from './SparklineAreaChart.vue'
+import SparklineAreaChart from './SparklineAreaChart.vue';
 
 const props = defineProps({
   title: {
     type: String,
-    default: ''
+    default: '',
   },
   value: {
     type: Number,
-    default: null
+    default: null,
   },
   fixed: {
     type: Number,
-    default: 2
-  }
+    default: 2,
+  },
 });
 
 const chartData = ref(props.value ? [props.value] : [0, 100, 0]);
+const max = ref(null);
+const min = ref(null);
 let lastUpdate = null;
-let reachedLength = false
+let reachedLength = false;
+let minMaxDay = null;
 
 const pushValue = (val, lastVal = 0) => {
   if (val === null) return;
@@ -39,6 +49,16 @@ const pushValue = (val, lastVal = 0) => {
 
   lastUpdate = new Date();
 
+  const today = lastUpdate.getDay();
+  if (minMaxDay !== today) {
+    minMaxDay = today;
+    max.value = val;
+    min.value = val;
+  }
+
+  max.value = Math.max(max.value, val);
+  min.value = Math.min(min.value, val);
+
   if (reachedLength) {
     chartData.value = [...chartData.value.slice(1), val];
     return;
@@ -46,55 +66,56 @@ const pushValue = (val, lastVal = 0) => {
 
   if (chartData.value.length > 10) {
     reachedLength = true;
-    return pushValue(val, lastVal)
+    return pushValue(val, lastVal);
   }
 
   chartData.value = [...chartData.value, val];
-}
+};
 
-watch(() => props.value, pushValue)
+watch(() => props.value, pushValue);
 
-watch(() => heartbeat.value, (current) => {
-  if (!lastUpdate) return;
-  if (current - lastUpdate >= 60000) {
-    const mostRecent = chartData.value[chartData.value.length - 1];
-    if (!chartData.value.every(v => v === mostRecent)) {
-      pushValue(mostRecent);
+watch(
+  () => heartbeat.value,
+  (current) => {
+    if (!lastUpdate) return;
+    if (current - lastUpdate >= 60000) {
+      const mostRecent = chartData.value[chartData.value.length - 1];
+      if (!chartData.value.every((v) => v === mostRecent)) {
+        pushValue(mostRecent);
+      }
     }
   }
-})
-
+);
 </script>
 <style scoped>
-  section {
-    color: var(--primary-color);
-    font-size: 120px;
-    padding: 8px;
-    text-align: right;
-    font-family: 'Roboto Condensed', sans-serif;
-    white-space: nowrap;
-    position: relative;
-  }
-  header {
-    font-family: 'Roboto', sans-serif;
-    font-size: 30px;
-    text-align: left;
-    padding: 0;
-  }
+section {
+  color: var(--primary-color);
+  font-size: 120px;
+  padding: 8px;
+  text-align: right;
+  font-family: 'Roboto Condensed', sans-serif;
+  white-space: nowrap;
+  position: relative;
+}
+header {
+  font-family: 'Roboto', sans-serif;
+  font-size: 30px;
+  text-align: left;
+  padding: 0;
+}
 
-  div {
-    position: relative;
-    z-index: 2;
-    -webkit-text-stroke-color: var(--background-color);
-    -webkit-text-stroke-width: 4px;
-    opacity: 0.9;
-  }
+div {
+  position: relative;
+  z-index: 2;
+  -webkit-text-stroke-color: var(--background-color);
+  -webkit-text-stroke-width: 4px;
+  opacity: 0.9;
+}
 
-  .Stat_Chart {
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-  }
-
+.Stat_Chart {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+}
 </style>
